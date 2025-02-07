62c320d31bf78a304f19bd225e2c7fecb1cf408c
Hebert S. Pereira <hbrt.spx@gmail.com>
2025-02-07 06:09:59 -0300

    feat: add task and project modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { APP_PIPE } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
+import { ProjectsModule } from './projects/projects.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, ProjectsModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
